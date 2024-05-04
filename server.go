779adefec141b326692783fa3a33f242bfc2ca8b
LeAnne B <branchleannelmb@gmail.com>
2024-05-04 15:57:05 -0400

    sets up basic http stuffs

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is the website. The whole thing. Really.\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Haiiiii\n")
+}
